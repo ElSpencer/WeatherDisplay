cdf2163f2f145c6a0055e4ce47436a193f4cd40f
Elinor Spencer <elinor.spencer@york.ac.uk>
2025-07-21 16:36:22 +0100

    added examples from pimoroni repo

---
+++ examples/textsize.py
@@ -0,0 +1,6 @@
+from PIL import Image, ImageDraw, ImageFont
+def textsize(text,font):
+	im = Image.new(mode="P",size=(0,0))
+	draw=ImageDraw.Draw(im)
+	_,_, width, height = draw.textbbox((0,0), text=text, font=font)
+	return width, height
